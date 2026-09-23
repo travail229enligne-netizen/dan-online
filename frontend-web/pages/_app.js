1d695acf7787da511dc1fe75fcdeab8fb8e86bec
Abd_satarou <abdsatarou@gmail.com>
2026-09-23 13:22:22 +0100

    Suppression complete du splash screen (composant, image, references)

--- frontend-web/pages/_app.js
+++ frontend-web/pages/_app.js
@@ -1,33 +1,13 @@
-import { useState, useEffect } from "react";
 import "../styles/globals.css";
 import { AuthProvider } from "../lib/auth";
 import { CartProvider } from "../lib/cart";
 import CartBar from "../components/CartBar";
 import PaymentWatcher from "../components/PaymentWatcher";
-import SplashScreen from "../components/SplashScreen";
-
-const SPLASH_KEY = "shopyz_splash_shown";
 
 export default function App({ Component, pageProps }) {
-  const [showSplash, setShowSplash] = useState(false);
-
-  useEffect(() => {
-    if (typeof window !== "undefined" && !sessionStorage.getItem(SPLASH_KEY)) {
-      setShowSplash(true);
-    }
-  }, []);
-
-  const handleSplashFinish = () => {
-    if (typeof window !== "undefined") {
-      sessionStorage.setItem(SPLASH_KEY, "1");
-    }
-    setShowSplash(false);
-  };
-
   return (
     <AuthProvider>
       <CartProvider>
-        {showSplash && <SplashScreen onFinish={handleSplashFinish} />}
         <PaymentWatcher />
         <Component {...pageProps} />
         <CartBar />
